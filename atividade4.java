adf91292eb064a8a4ad2532bae830f2c56ab4f9a
Nik4o6 <nicolashenriquec583@gmail.com>
2026-01-30 22:05:17 -0300

    exercicio trashtalk

---
+++ atividade4.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class atividade4 {
+    public static void main(String[] args) {
+
+        double valor = 12.34567;
+
+        System.out.printf("%.2f%n", valor);
+    }
+}
---
+++ atividade5.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class atividade5 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int soma = a + b;
+
+        System.out.println(soma);
+    }
+}
