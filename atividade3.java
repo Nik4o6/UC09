1c5d4f7dd16c262554fea271caf6df92d25ef087
Nik4o6 <nicolashenriquec583@gmail.com>
2026-01-30 21:28:12 -0300

    passando raiva

--- atividade3.java
+++ atividade3.java
@@ -1,15 +1,18 @@
-
 import java.util.Scanner;
 
 public class atividade3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite o preço: ");
         double preco = sc.nextDouble();
+
+        System.out.print("Digite a quantidade: ");
         int quantidade = sc.nextInt();
 
         double custoTotal = preco * quantidade;
+        System.out.println("Custo total: " + custoTotal);
 
-        System.out.println(custoTotal);
+        sc.close();
     }
 }
