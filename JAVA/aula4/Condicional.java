e5747cddf758b584a0f60828be350201c1bd194d
Nik4o6 <nicolashenriquec583@gmail.com>
2026-02-02 19:45:35 -0300

    .

---
+++ JAVA/aula4/Condicional.java
@@ -0,0 +1,14 @@
+package aula4;
+ 
+public class Condicional {
+   
+    public static void main(String[] args) {
+       
+        int x = 10;
+        int y = 10;
+        if ( x == y ){
+ 
+    System.out.println("X é igual a Y");
+        } 
+    }
+}
